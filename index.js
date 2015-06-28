4bdb1fe1bc371c771d5884ef1fc5380564a0d97a
kazu69 <kazu69web@gmail.com>
2015-06-28 11:46:28 +0900

    fixed include path

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict';
-hexo.extend.filter.register('post_permalink', require('./lib/permalink_normalize'));
+hexo.extend.filter.register('post_permalink', require('./lib/permalink_normalization'));
